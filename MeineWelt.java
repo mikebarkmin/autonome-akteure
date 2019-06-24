6b7223ef0ec83d657c6a63a2bcfd70e176095945
Mike Barkmin <mike.barkmin@uni-due.de>
2019-06-24 12:45:44 +0200

    Move MeineWelt init to constructor

--- MeineWelt.java
+++ MeineWelt.java
@@ -12,34 +12,34 @@ class MeineWelt extends Welt {
 
     this.setzeKameraPosition(91,255,84);
     this.setzeKameraBlickpunkt(85,-58,88);
-  }
-
-  public static void main(String[] args) {
-    Welt s = new MeineWelt();
 
     new GLHimmel();
     new GLBoden();
 
     for(int x = 0; x < LAENGE; x++) {
-      s.fuegeObjektHinzu(new Wand(), x, 0, 0);
-      s.fuegeObjektHinzu(new Wand(), x, 0, BREITE);
+      this.fuegeObjektHinzu(new Wand(), x, 0, 0);
+      this.fuegeObjektHinzu(new Wand(), x, 0, BREITE);
     }
 
     for(int y = 0; y < BREITE; y++) {
-      s.fuegeObjektHinzu(new Wand(), 0, 0, y);
-      s.fuegeObjektHinzu(new Wand(), LAENGE, 0, y);
+      this.fuegeObjektHinzu(new Wand(), 0, 0, y);
+      this.fuegeObjektHinzu(new Wand(), LAENGE, 0, y);
     }
 
-    s.fuegeObjektHinzu(new StaubsaugRoboter(), 5, 0, 5);
-    s.fuegeObjektHinzu(new Wand(), 8, 0, 5);
-    s.fuegeObjektHinzu(new Wand(), 7, 0, 4);
+    this.fuegeObjektHinzu(new StaubsaugRoboter(), 5, 0, 5);
+    this.fuegeObjektHinzu(new Wand(), 8, 0, 5);
+    this.fuegeObjektHinzu(new Wand(), 7, 0, 4);
 
-    s.fuegeObjektHinzu(new Staub(), 2, 0, 2);
-    s.fuegeObjektHinzu(new Staub(), 2, 0, 4);
-    s.fuegeObjektHinzu(new Staub(), 4, 0, 8);
-    s.fuegeObjektHinzu(new Staub(), 7, 0, 2);
-    s.fuegeObjektHinzu(new Staub(), 3, 0, 2);
-    s.fuegeObjektHinzu(new Staub(), 5, 0, 4);
+    this.fuegeObjektHinzu(new Staub(), 2, 0, 2);
+    this.fuegeObjektHinzu(new Staub(), 2, 0, 4);
+    this.fuegeObjektHinzu(new Staub(), 4, 0, 8);
+    this.fuegeObjektHinzu(new Staub(), 7, 0, 2);
+    this.fuegeObjektHinzu(new Staub(), 3, 0, 2);
+    this.fuegeObjektHinzu(new Staub(), 5, 0, 4);
+  }
+
+  public static void main(String[] args) {
+    Welt s = new MeineWelt();
     s.starte();
   }
 }
